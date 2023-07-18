7caf1c5b82ceb2c9c0c1ba61e98fa40cd6983b52
Daniel Kim <78152949+dannykim1997@users.noreply.github.com>
2023-07-18 16:29:45 -0700

    Create function that plays round and gives result

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let randomComputerChoice = Math.floor(Math.random() * 3) + 1;
 let computerHand;
 
 function getComputerChoice() {
-    if(randomComputerChoice == 1) {
+    if (randomComputerChoice == 1) {
         computerHand = "Rock";
     } else if (randomComputerChoice == 2) {
         computerHand = "Paper";
@@ -13,4 +13,28 @@ function getComputerChoice() {
     return computerHand
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "Rock") {
+        return "You tied, rock ties rock";   
+    } else if (playerSelection == "rock" && computerSelection == "Paper") {
+        return "You lose, rock loses to paper";
+    } else if (playerSelection == "rock" && computerSelection == "Scissors") {
+        return "You win, rock beats scissors";  
+    } else if (playerSelection == "paper" && computerSelection == "Rock") {
+        return "You lose, paper loses to rock"
+    } else if (playerSelection == "paper" && computerSelection == "Paper") {
+        return "You tie, paper ties paper";   
+    } else if (playerSelection == "paper" && computerSelection == "Scissors") {
+        return "You lose, paper loses to scissors";   
+    } else if (playerSelection == "scissors" && computerSelection == "Rock") {
+        return "You lose, scissors loses to rock";   
+    } else if (playerSelection == "scissors" && computerSelection == "Paper") {
+        return "You win, scissors beats paper";   
+    } else if (playerSelection == "scissors" && computerSelection == "Scissors") {
+        return "You tie, scissors ties scissors";   
+    }
+}
+
+let playerSelection = prompt("Enter rock, paper, or scissors").toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
